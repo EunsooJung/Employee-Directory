56a2670f3f4e925e4282b34ff2ade977f9901a11
EunsooJung <hi.michael.jung@gmail.com>
2020-02-06 02:46:49 -0800

    Created Navigation Componet prototype

---
+++ src/components/Navigation.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Button } from 'antd';
+import 'antd/dist/antd.css';
+
+function Navigation() {
+  return (
+    <nav className='navbar navbar-expand-lg navbar-light bg-light'>
+      <Button
+        className='navbar-toggler'
+        type='button'
+        data-toggle='collapse'
+        data-target='#navbarNav'
+        aria-controls='navbarNav'
+        aria-expanded='false'
+        aria-label='Toggle navigation'
+      >
+        <span className='navbar-toggler-icon' />
+      </Button>
+      <div className='collapse navbar-collapse row' id='navbarNav'>
+        {/* inpup text to search by name */}
+        <div className='search-area col-4'>
+          {/* <SearchByName /> */}Search Name
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navigation;
