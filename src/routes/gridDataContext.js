f942325738835d730af590f07413ea0ab5d84d8a
EunsooJung <hi.michael.jung@gmail.com>
2020-02-06 02:24:29 -0800

    Updated Context

--- src/routes/gridDataContext.js
+++ src/routes/gridDataContext.js
@@ -2,6 +2,8 @@
  * React Context
  * Context provides a way to pass data through the component tree without
  * having to pass props down manually at every level.
+ *
+ * https://reactjs.org/docs/context.html#reactcreatecontext
  */
 
 import React from 'react';
