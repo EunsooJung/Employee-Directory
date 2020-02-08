b3ed20632af1f00c9e425ea60dfb78244a3c4414
EunsooJung <hi.michael.jung@gmail.com>
2020-02-07 21:21:55 -0800

    Updated Header style

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,11 +4,11 @@ import '../styles/Header.css';
 function Header() {
   return (
     <div className='header'>
-      <h1>React Empoloyee Directory</h1>
-      <p>
+      <h1 className='h-h1'>React Empoloyee Directory</h1>
+      <h6 className='h6-1'>
         Click on grid title to filter by heading or use the search box to find
         your results.
-      </p>
+      </h6>
     </div>
   );
 }
